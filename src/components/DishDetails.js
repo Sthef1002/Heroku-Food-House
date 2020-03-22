0a5e755ccded1906f83efe3ff9b7c30a3175d38d
Sthefany Azevedo <Sthefany@Sthefanys-MacBook-Pro-4.local>
2020-03-21 21:03:29 -0400

    Recipe details page

--- src/components/DishDetails.js
+++ src/components/DishDetails.js
@@ -1,39 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import axios from 'axios';
-import Card from 'react-bootstrap/Card'
+import './DishDetails.css'
 
 const DishDetails = props => {
 
+  const [recipe, setRecipe] = useState({ meals: [] })
+  const getRecipe = async () => {
+      const { data } = await axios(
+        `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${props.match.params.id}`
+      );
+      setRecipe({ meals: data.meals[0] });
+    };
 
-
-    const [recipe, setRecipe] = useState({ meals: [] })
- 
-    const getRecipe = async () => {
-        const { data } = await axios(
-          `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${props.match.params.id}`
-        );
-        setRecipe({ meals: data.meals[0] });
-      };
- 
-      useEffect(() => {
-        getRecipe();
-      }, []); 
-
-      console.log(recipe);
-     
-    return (
-        <div>   <p>{recipe.meals.strMeal}</p>
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={recipe.meals.strMealThumb} alt={recipe.meals.strMeal}  />
-            <Card.Body>
-                <Card.Title>{recipe.meals.strMeal}</Card.Title>
-                <Card.Text>
-                     {recipe.meals.strInstructions}
-                </Card.Text>
-            </Card.Body>
-        </Card>
+    useEffect(() => {
+      getRecipe();
+    }, []); 
+    
+  return (
+    <div className="details">
+      <div id='line-nav'>
+      </div>   
+      <div id='left-details'>
+        <h4 id='title-details'>{recipe.meals.strMeal}</h4>
+        <h5>PREPARATION</h5>
+        <p id='recipe-text'>{recipe.meals.strInstructions}</p>
       </div>
-    )
+      <div id='right-details'>
+        <img 
+          alt={recipe.meals.strMeal} 
+          id='picture-dish'
+          src={recipe.meals.strMealThumb} 
+        />
+      </div>
+    </div>
+  )
 }
 
 export default DishDetails;
