4dc10e5f830d35f7f94f074f1404b1d46140defe
Sthefany Azevedo <Sthefany@Sthefanys-MacBook-Pro-4.local>
2020-03-22 10:40:32 -0400

    Recipe of the day details page

--- src/components/RandomRecipe.js
+++ src/components/RandomRecipe.js
@@ -6,41 +6,39 @@ import "./RandomRecipe.css";
 
 
 const RandomRecipe = () => {
-    const [ random, setRandom ] = useState({ meals: []})
-    const getRandom = async () => {
-        const { data } = await axios(
-          `https://www.themealdb.com/api/json/v1/1/random.php`
-        );
-        setRandom({ meals: data.meals[0] });
-      };
- 
-      useEffect(() => {
-        getRandom();
-      }, []);
-      console.log(random);
-    return (
+  
+  const [ random, setRandom ] = useState({ meals: []})
+  const getRandom = async () => {
+      const { data } = await axios(
+        `https://www.themealdb.com/api/json/v1/1/random.php`
+      );
+      setRandom({ meals: data.meals[0] });
+    };
 
-        <div className="randomContainer">
-        {/* <Card className="randomCard" style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={random.meals.strMealThumb} alt={random.meals.strMeal}  />
-        <Card.Body>
-          <Card.Title>{random.meals.strMeal}</Card.Title>
-          <Card.Text>
-          {random.meals.strInstructions}
-          </Card.Text>
-        </Card.Body>
-      </Card> */}
-      <Card>
-    <Card.Img variant="top" src={random.meals.strMealThumb} alt={random.meals.strMeal}  />
-    <Card.Body>
-    <Card.Title>{random.meals.strMeal}</Card.Title>
-          <Card.Text>
-          {random.meals.strInstructions}
-          </Card.Text>
-    </Card.Body>
-  </Card>
+    useEffect(() => {
+      getRandom();
+    }, []);
+    console.log(random);
+  return (
+
+
+    <div className="details">
+      <div id='line-nav'>
+      </div>   
+      <div id='left-details'>
+        <h4 id='title-details'>{random.meals.strMeal}</h4>
+        <h5>PREPARATION</h5>
+        <p id='recipe-text'>{random.meals.strInstructions}</p>
       </div>
-    )
+      <div id='right-details'>
+        <img 
+          alt={random.meals.strMeal} 
+          id='picture-dish'
+          src={random.meals.strMealThumb}
+        />
+      </div>
+    </div>
+  )
 }
 export default RandomRecipe;
 
